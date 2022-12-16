18da66c03138d5f41d73c220d5fd5d82cd6a6063
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2022-12-16 13:16:19 -0500

    test: Add tests for IconComponents (#11)

---
+++ test/alerts_viewer_web/components/icon_components_test.exs
@@ -0,0 +1,30 @@
+defmodule AlertsViewerWeb.IconComponentsTest do
+  use ExUnit.Case, async: true
+
+  import AlertsViewerWeb.IconComponents
+
+  describe "icon/1" do
+    test "generates an svg or a stylized span element for the requested icon type" do
+      assert_rendered(icon(%{type: "Red"}), "<svg")
+      assert_rendered(icon(%{type: "Blue"}), "<svg")
+      assert_rendered(icon(%{type: "Orange"}), "<svg")
+      assert_rendered(icon(%{type: "Green-B"}), "<svg")
+      assert_rendered(icon(%{type: "Green-C"}), "<svg")
+      assert_rendered(icon(%{type: "Green-D"}), "<svg")
+      assert_rendered(icon(%{type: "Green-D"}), "<svg")
+      assert_rendered(icon(%{type: "Green"}), "<svg")
+      assert_rendered(icon(%{type: "Mattapan"}), "<svg")
+      assert_rendered(icon(%{type: "using_escalator"}), "<svg")
+      assert_rendered(icon(%{type: "using_wheelchair"}), "<svg")
+      assert_rendered(icon(%{type: "park_car"}), "<svg")
+      assert_rendered(icon(%{type: "bike"}), "<svg")
+      assert_rendered(icon(%{type: "CR-Haverhill"}), "<span")
+      assert_rendered(icon(%{type: "131"}), "<span")
+    end
+  end
+
+  defp assert_rendered(result, pattern) do
+    assert %Phoenix.LiveView.Rendered{static: static} = result
+    assert Enum.any?(static, fn output -> output =~ pattern end)
+  end
+end
