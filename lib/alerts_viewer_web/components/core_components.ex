f176990968bb74458e0eea0509b582ef18ce4093
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2022-12-20 15:56:15 -0500

    style: Remove padding on the select component
    Options with long names were overlapping with the disclosure triangle.

--- lib/alerts_viewer_web/components/core_components.ex
+++ lib/alerts_viewer_web/components/core_components.ex
@@ -305,7 +305,7 @@ defmodule AlertsViewerWeb.CoreComponents do
       <select
         id={@id}
         name={@name}
-        class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-zinc-500 focus:border-zinc-500 sm:text-sm"
+        class="mt-1 block w-full border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-zinc-500 focus:border-zinc-500 sm:text-sm"
         multiple={@multiple}
         {@rest}
       >
